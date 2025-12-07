238f03f28a618f3970f5a74a9db768d4945918f7
Salvogrc <salvogeraci030899@gmail.com>
2025-12-07 13:05:20 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{ 
+      background: 'black', 
+      color: 'white',
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    }}>
+      <h1>Market Cube Pro - Loading...</h1>
+    </div>
+  );
+}
